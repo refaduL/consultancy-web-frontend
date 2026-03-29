a3c12cdef178f760e569b67be893362aea817748
refaduL <refadul.cse@gmail.com>
2026-03-29 11:41:51 +0600

    fix: home page navigation issue

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
         <Route path="services" element={<Services />} />
         <Route path="universities" element={<Universities />} />
         <Route path="about" element={<About />} />
